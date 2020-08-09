7e8998183eee65239585350bc857b353c34e900f
J <j.e.aten@gmail.com>
2020-08-09 11:53:45 -0500

    try harder to avoid panic: cgo argument has Go pointer to Go pointer

--- lmdb/cursor.go
+++ lmdb/cursor.go
@@ -188,7 +188,9 @@ func (c *Cursor) Get(setkey, setval []byte, op uint) (key, val []byte, err error
 //
 // See mdb_cursor_get.
 func (c *Cursor) getVal0(op uint) error {
-	ret := C.mdb_cursor_get(c._c, (*C.MDB_val)(unsafe.Pointer(c.txn.key)), (*C.MDB_val)(unsafe.Pointer(c.txn.val)), C.MDB_cursor_op(op))
+	key := c.txn.key
+	val := c.txn.val
+	ret := C.mdb_cursor_get(c._c, key, val, C.MDB_cursor_op(op))
 	return operrno("mdb_cursor_get", ret)
 }
 
@@ -197,10 +199,12 @@ func (c *Cursor) getVal0(op uint) error {
 //
 // See mdb_cursor_get.
 func (c *Cursor) getVal1(setkey []byte, op uint) error {
+	key := c.txn.key
+	val := c.txn.val
 	ret := C.lmdbgo_mdb_cursor_get1(
 		c._c,
 		(*C.char)(unsafe.Pointer(&setkey[0])), C.size_t(len(setkey)),
-		(*C.MDB_val)(unsafe.Pointer(c.txn.key)), (*C.MDB_val)(unsafe.Pointer(c.txn.val)),
+		key, val,
 		C.MDB_cursor_op(op),
 	)
 	return operrno("mdb_cursor_get", ret)
@@ -211,11 +215,13 @@ func (c *Cursor) getVal1(setkey []byte, op uint) error {
 //
 // See mdb_cursor_get.
 func (c *Cursor) getVal2(setkey, setval []byte, op uint) error {
+	key := c.txn.key
+	val := c.txn.val
 	ret := C.lmdbgo_mdb_cursor_get2(
 		c._c,
 		(*C.char)(unsafe.Pointer(&setkey[0])), C.size_t(len(setkey)),
 		(*C.char)(unsafe.Pointer(&setval[0])), C.size_t(len(setval)),
-		c.txn.key, c.txn.val,
+		key, val,
 		C.MDB_cursor_op(op),
 	)
 	return operrno("mdb_cursor_get", ret)
